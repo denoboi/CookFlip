7d336db87ac2d142a0032e793501fea295ee19dd
denoboi <deniz.kaymakci@hotmail.com>
2022-02-07 14:11:38 +0300

    DoughTrigger

--- Assets/[Game]/Scripts/DoughTrigger.cs
+++ Assets/[Game]/Scripts/DoughTrigger.cs
@@ -1,18 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using HCB.Core;
+using HCB.SplineMovementSystem;
 
 public class DoughTrigger : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private void OnTriggerEnter(Collider other)
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+            Debug.Log("DoughSpilled");
     }
 }
