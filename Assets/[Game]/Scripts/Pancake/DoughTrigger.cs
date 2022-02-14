57d51e2475b1ec34af927fadcca7088cc570da62
denoboi <deniz.kaymakci@hotmail.com>
2022-02-14 21:35:45 +0300

    Disable First Dough

--- Assets/[Game]/Scripts/Pancake/DoughTrigger.cs
+++ Assets/[Game]/Scripts/Pancake/DoughTrigger.cs
@@ -4,18 +4,42 @@ using UnityEngine;
 
 public class DoughTrigger : MonoBehaviour
 {
+    //[SerializeField] private GameObject pancakeDough;
     [SerializeField] private GameObject dough;
+    [SerializeField] private float delay;
+
+
     private void OnTriggerEnter(Collider other)
     {
 
         Debug.Log("DoughSpilled");
         if (other.CompareTag("PancakeDoughBone"))
             EnableDough();
+        
+        
+        DisableDough();
+       
     }
+
+    
 
 
     public void EnableDough()
     {
         dough.SetActive(true);
+    }
+
+    public void DisableDough()
+    {
+        StartCoroutine(DisableCo(dough));
+    }
+
+    private IEnumerator DisableCo(GameObject dough)
+    {
+        if(dough.activeSelf)
+        {
+            yield return new WaitForSeconds(delay);
+            dough.SetActive(false);
+        }
     }
 }
