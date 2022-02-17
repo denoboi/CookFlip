6ea7dea47dbef184717929ddb79c1f2b7e9ad202
denoboi <deniz.kaymakci@hotmail.com>
2022-02-17 16:54:30 +0300

    Pancake Throw now Broken :-1:

---
+++ Assets/[Game]/Scripts/Pancake/PancakeThrowLeft.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PancakeThrowLeft : MonoBehaviour
+{
+
+    private Animator PancakeLeftAnim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PancakeLeftAnim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlateAnimation();
+    }
+
+    public void PlateAnimation()
+    {
+        if (Input.GetMouseButtonUp(0) && PancakeStats.Instance.isCooked == true)
+        {
+            PancakeLeftAnim.SetTrigger("PancakeLeftAnim");
+        }
+        
+    }
+}
